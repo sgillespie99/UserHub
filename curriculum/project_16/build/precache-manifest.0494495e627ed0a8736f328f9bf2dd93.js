394ca72509ae03cdc4796d5bb8339523e7af5469
Sebastian <SebastianGillespie99@gmail.com>
2020-08-29 10:31:13 -0400

    finished project

---
+++ curriculum/project_16/build/precache-manifest.0494495e627ed0a8736f328f9bf2dd93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "588736d90457c1bccaed432f908e5fa1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2afda148a85ac8f45ed0",
+    "url": "/static/css/main.1253bac1.chunk.css"
+  },
+  {
+    "revision": "f98c157011c80e910a79",
+    "url": "/static/js/2.4776ca5c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.4776ca5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2afda148a85ac8f45ed0",
+    "url": "/static/js/main.6a318945.chunk.js"
+  },
+  {
+    "revision": "e44ca8050686401cca12",
+    "url": "/static/js/runtime-main.13213274.js"
+  }
+]);
